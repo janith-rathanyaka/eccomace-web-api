87b10408d51e3cc93a0fd2d8114a39f67335f71f
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2021-06-19 13:04:33 +0530

    update products routes

--- routers/products.js
+++ routers/products.js
@@ -2,6 +2,7 @@ const {Product} = require('../models/product')
 const express = require('express');
 const { Category } = require('../models/category');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 router.get(`/`,async (req,res)=>{
     const productlist = await Product.find();
@@ -11,31 +12,97 @@ router.get(`/`,async (req,res)=>{
     res.send(productlist);
 })
 
-router.post(`/`,async (req,res)=>{
-    const category = await Category.findById(req.body.category);
-    if(!category) return res.status(400).send('Invalid category')
+router.get(`/:id`, async (req,res)=>{
+    const product = await Product.findById(req.params.id);
 
-   const product = new Product({
-       name:req.body.name,
-       description:req.body.description,
-       richDescription:req.body.richDescription,
-       image:req.body.image,
-       brand:req.body.brand,
-       price:req.body.price,
-       category:req.body.category,
-       countInStock:req.body.countInStock,
-       rating:req.body.rating,
-       numReviews:req.body.numReviews,
-       isFeatured:req.body.isFeatured
-
-   })    
-   
-   product = await product.save();
-   
-   if(!product)
-   return res.status(500).send('The product cannot be creaetd')
-   
-   res.send(product)
+    if(!product){
+        res.status(500).json({success:false})
+    }
+    res.send(product)
 })
+
+
+
+router.post(`/`,async (req,res,next)=>{
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category')
+
+    let product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+    })
+
+    product = await product.save();
+
+    if(!product) 
+    return res.status(500).send('The product cannot be created')
+
+    res.send(product);
+})
+
+
+router.put('/:id',async (req,res)=>{
+    if(!mongoose.isValidObjectId(req.params.id)){
+        res.status(400).send('Invalid Product Id')
+    }
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category')
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+        },
+        {new:true}
+    )
+
+    if(!product)
+    return res.send(404).send('the category cannot be updated')
+
+    res.send(product);
+})
+
+router.delete('/:id', (req,res)=>{
+    Product.findByIdAndRemove(req.params.id).then(product=>{
+        if(product){
+            return res.status(200).json({success:true, message:'the category deleted'})
+        }else{
+            return res.status(404).json({success:false,message:"category not founded"})
+        }
+    }).catch(err=>{
+        return res.status(400).json({success:false,error:err})
+    })
+})
+
+router.get('/get/count',async (req,res)=>{
+    const productCount = await Product.countDocuments((count)=>count)
+
+    if(!productCount){
+        res.status(500).json({success:false})
+    }
+    res.send({
+        productCount:productCount
+    });
+})
+
 
 module.exports = router;
